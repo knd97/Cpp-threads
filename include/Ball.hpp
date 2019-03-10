bb77e19ce90c1c2a8434f7896bb9dd45077c189c
knd47 <k.olszewski47@gmail.com>
2019-03-10 17:23:03 +0100

    change name of Ball attribute

--- include/Ball.hpp
+++ include/Ball.hpp
@@ -12,7 +12,7 @@ class Ball
         std::pair<int, int>coordinates_;        //(x, y) position of ball
         std::thread ball_thread_;
         std::chrono::milliseconds interval_;
-        const char ball_symbol = 'o';
+        const char ball_symbol_ = 'o';
 
     public:
         Ball(std::pair<int, int>position, std::chrono::milliseconds period);
