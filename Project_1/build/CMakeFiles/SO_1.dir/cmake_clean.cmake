cc998a076d0f8261f5eb69be44d53473cbb24f5c
knd47 <k.olszewski47@gmail.com>
2019-04-08 13:22:54 +0200

    improve project structure

---
+++ Project_1/build/CMakeFiles/SO_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SO_1.dir/src/Ball.cpp.o"
+  "CMakeFiles/SO_1.dir/src/Screen.cpp.o"
+  "CMakeFiles/SO_1.dir/src/main.cpp.o"
+  "SO_1.pdb"
+  "SO_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SO_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
