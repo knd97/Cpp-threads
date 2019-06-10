f31d45b446b223b0adf61f171bee4122d4ea293a
knd47 <k.olszewski47@gmail.com>
2019-06-10 02:43:20 +0200

    worker template

--- Project_3/include/Worker.hpp
+++ Project_3/include/Worker.hpp
@@ -1,8 +1,28 @@
 #pragma once
 
+#include <thread>
+#include "SeaPort.hpp"
+#include "Window.hpp"
+
 class Worker
 {
+private:
+    static const std::pair<int, int> starting_poit_;
+    std::thread worker_thread_;
+    std::shared_ptr<SeaPort> ramps_;
+    std::shared_ptr<Window> main_window_;
+    std::pair<int, int> coordinates_;
+    std::pair<int, int> previous_coordinates_;
+    std::chrono::milliseconds speed_;
+
 public:
+    Worker(std::pair<int, int> coordinates, std::shared_ptr<SeaPort> ramps, std::shared_ptr<Window> main_window);
+    Worker(const Worker &) = delete;
+    Worker &operator=(const Worker &) = delete;
+    Worker(Worker &&) = delete;
+    Worker &operator=(Worker &&) = delete;
+    ~Worker();
+
     void stop();
     void start();
 };
---
+++ Project_3/src/Worker.cpp
@@ -0,0 +1,1 @@
+#include "../include/Worker.hpp"
